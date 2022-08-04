fd8eba9dde8673faf7ccd0de8ca4f42671744e1f
Bruno Guimaraes <devgmrs@gmail.com>
2022-08-04 15:57:49 -0300

    :wrench: chore: corrgindo o service client

--- services/cliente-service.js
+++ services/cliente-service.js
@@ -1,7 +1,15 @@
 async function listaClientes() {
-	const url = await fetch('http://localhost:3000/profile');
-	const urlPara = await url.json();
-	return urlPara;
+	try {
+		const url = await fetch('http://localhost:3000/profile');
+		let urlPara;
+		if (url.status === 200) {
+			urlPara = await url.json();
+		}
+		console.log(urlPara);
+		return urlPara;
+	} catch (e) {
+		throw new Error('Falha na conexão');
+	}
 }
 
 const ClienteService = {
